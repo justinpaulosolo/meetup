9512d3cab9786b4c40ef692105618da157639857
justinpaulosolo <justinpaulosolo@gmail.com>
2022-10-06 04:23:43 -0700

    fix: Logo in register and login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ export default function Login() {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <Link href="/">
           <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            Hacktober
+            Meetup
           </a>
         </Link>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
--- pages/register.tsx
+++ pages/register.tsx
@@ -8,7 +8,7 @@ export default function Register() {
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          Hacktober
+          Meetup
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
